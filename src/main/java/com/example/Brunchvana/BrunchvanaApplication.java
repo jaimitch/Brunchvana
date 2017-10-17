67ab9f1384e50ffce753edb97f07121b109aa6d0
Jaren Mitchell <jaren_mitchell@yahoo.com>
2017-10-17 15:29:39 -0500

    first commit

---
+++ src/main/java/com/example/Brunchvana/BrunchvanaApplication.java
@@ -0,0 +1,12 @@
+package com.example.Brunchvana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunchvanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunchvanaApplication.class, args);
+	}
+}
