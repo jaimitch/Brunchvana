30922dce579bb393ab9dafa556800a8c0101e1d8
roxyfaye <rdesantisnunally@gmail.com>
2017-10-17 23:05:45 -0400

    front end files created

--- backend/src/main/java/com/example/Brunchvana/BrunchvanaApplication.java
+++ backend/src/main/java/com/example/Brunchvana/BrunchvanaApplication.java
@@ -9,4 +9,16 @@ public class BrunchvanaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BrunchvanaApplication.class, args);
 	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
 }
